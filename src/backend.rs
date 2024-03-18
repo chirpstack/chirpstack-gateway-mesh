75c71f722d210a074ad7861cc5e7bbff03c95989
Orne Brocaar <info@brocaar.com>
2024-03-18 13:52:45 +0000

    Refactor backend (zeromq > zmq crate).
    The zeromq create does not handle reconnects between ZMQ sockets well.

--- src/backend.rs
+++ src/backend.rs
@@ -1,21 +1,24 @@
-use std::time::Duration;
+use std::thread;
 
 use anyhow::Result;
 use chirpstack_api::prost::Message;
 use log::{debug, error, info, trace};
 use once_cell::sync::OnceCell;
-use tokio::{
-    sync::Mutex,
-    time::{sleep, timeout},
-};
-use zeromq::{Socket, SocketRecv, SocketSend};
+use tokio::sync::{mpsc, oneshot, Mutex};
 
 use crate::config::Configuration;
 use crate::{helpers, proxy, relay};
 use chirpstack_api::gw;
 
-static CONCENTRATORD: OnceCell<Mutex<Backend>> = OnceCell::new();
-static RELAY_CONCENTRATORD: OnceCell<Mutex<Backend>> = OnceCell::new();
+static GATEWAY_ID: OnceCell<Mutex<[u8; 8]>> = OnceCell::new();
+static RELAY_ID: OnceCell<Mutex<[u8; 4]>> = OnceCell::new();
+
+static CONCENTRATORD_CMD_CHAN: OnceCell<CommandChannel> = OnceCell::new();
+static RELAY_CONCENTRATORD_CMD_CHAN: OnceCell<CommandChannel> = OnceCell::new();
+
+type Event = (String, Vec<u8>);
+type Command = ((String, Vec<u8>), oneshot::Sender<Result<Vec<u8>>>);
+type CommandChannel = mpsc::UnboundedSender<Command>;
 
 pub async fn setup(conf: &Configuration) -> Result<()> {
     setup_concentratord(conf).await?;
@@ -29,23 +32,74 @@ async fn setup_concentratord(conf: &Configuration) -> Result<()> {
         conf.backend.concentratord.event_url, conf.backend.concentratord.command_url
     );
 
-    let mut event_sock = zeromq::SubSocket::new();
-    event_sock
-        .connect(&conf.backend.concentratord.event_url)
-        .await?;
-    event_sock.subscribe("").await?;
+    // Setup ZMQ command.
 
-    let mut cmd_sock = zeromq::ReqSocket::new();
-    cmd_sock
-        .connect(&conf.backend.concentratord.command_url)
-        .await?;
+    // As the zmq::Context can't be shared between threads, we use a channel.
+    let (cmd_tx, mut cmd_rx) = mpsc::unbounded_channel::<Command>();
 
-    let mut b = Backend {
-        cmd_sock,
-        gateway_id: None,
-    };
-    b.read_gateway_id().await?;
+    // Spawn the zmq command handler to a dedicated thread.
+    thread::spawn({
+        let command_url = conf.backend.concentratord.command_url.clone();
 
+        move || {
+            let zmq_ctx = zmq::Context::new();
+            let mut sock = zmq_ctx.socket(zmq::REQ).unwrap();
+            sock.connect(&command_url).unwrap();
+
+            while let Some(cmd) = cmd_rx.blocking_recv() {
+                let resp = send_zmq_command(&mut sock, &cmd);
+                cmd.1.send(resp).unwrap();
+            }
+
+            error!("Concentratord command loop has been interrupted");
+        }
+    });
+
+    // Read Gateway ID.
+
+    trace!("Reading Gateway ID");
+    let mut gateway_id: [u8; 8] = [0; 8];
+    let (gateway_id_tx, gateway_id_rx) = oneshot::channel::<Result<Vec<u8>>>();
+    cmd_tx.send((("gateway_id".to_string(), vec![]), gateway_id_tx))?;
+    let resp = gateway_id_rx.await??;
+    gateway_id.copy_from_slice(&resp);
+    info!("Retrieved Gateway ID: {}", hex::encode(gateway_id));
+    GATEWAY_ID
+        .set(Mutex::new(gateway_id))
+        .map_err(|e| anyhow!("OnceCell error: {:?}", e))?;
+
+    // Set CMD channel.
+
+    CONCENTRATORD_CMD_CHAN
+        .set(cmd_tx)
+        .map_err(|e| anyhow!("OnceCell error: {:?}", e))?;
+
+    // Setup ZMQ event.
+
+    let (event_tx, event_rx) = mpsc::unbounded_channel::<Event>();
+
+    // Spawn the zmq event handler to a dedicated thread.
+    thread::spawn({
+        let event_url = conf.backend.concentratord.event_url.clone();
+
+        move || {
+            let zmq_ctx = zmq::Context::new();
+            let mut sock = zmq_ctx.socket(zmq::SUB).unwrap();
+            sock.connect(&event_url).unwrap();
+            sock.set_subscribe("".as_bytes()).unwrap();
+
+            loop {
+                match receive_zmq_event(&mut sock) {
+                    Ok(v) => event_tx.send(v).unwrap(),
+                    Err(e) => {
+                        error!("Error receiving ZMQ event, error: {}", e);
+                    }
+                }
+            }
+        }
+    });
+
+    // Spawn event handler.
     tokio::spawn({
         let border_gateway = conf.relay.border_gateway;
         let border_gateway_ignore_direct_uplinks = conf.relay.border_gateway_ignore_direct_uplinks;
@@ -58,16 +112,12 @@ async fn setup_concentratord(conf: &Configuration) -> Result<()> {
             event_loop(
                 border_gateway,
                 border_gateway_ignore_direct_uplinks,
-                event_sock,
+                event_rx,
                 filters,
             )
             .await;
         }
     });
-
-    CONCENTRATORD
-        .set(Mutex::new(b))
-        .map_err(|_| anyhow!("OnceCell set error"))?;
 
     Ok(())
 }
@@ -78,112 +128,98 @@ async fn setup_relay_concentratord(conf: &Configuration) -> Result<()> {
         conf.backend.relay_concentratord.event_url, conf.backend.relay_concentratord.command_url
     );
 
-    let mut event_sock = zeromq::SubSocket::new();
-    event_sock
-        .connect(&conf.backend.relay_concentratord.event_url)
-        .await?;
-    event_sock.subscribe("").await?;
+    // Setup ZMQ command.
 
-    let mut cmd_sock = zeromq::ReqSocket::new();
-    cmd_sock
-        .connect(&conf.backend.relay_concentratord.command_url)
-        .await?;
+    // As the zmq::Context can't be shared between threads, we use a channel.
+    let (cmd_tx, mut cmd_rx) = mpsc::unbounded_channel::<Command>();
 
-    let mut b = Backend {
-        cmd_sock,
-        gateway_id: None,
-    };
-    b.read_gateway_id().await?;
+    // Spawn the zmq command handler to a dedicated thread.
+    thread::spawn({
+        let command_url = conf.backend.relay_concentratord.command_url.clone();
 
+        move || {
+            let zmq_ctx = zmq::Context::new();
+            let mut sock = zmq_ctx.socket(zmq::REQ).unwrap();
+            sock.connect(&command_url).unwrap();
+
+            while let Some(cmd) = cmd_rx.blocking_recv() {
+                let resp = send_zmq_command(&mut sock, &cmd);
+                cmd.1.send(resp).unwrap();
+            }
+
+            error!("Relay Concentratord command loop has been interrupted");
+        }
+    });
+
+    // Read Relay ID.
+    trace!("Reading Gateway ID");
+
+    let (gateway_id_tx, gateway_id_rx) = oneshot::channel::<Result<Vec<u8>>>();
+    cmd_tx.send((("gateway_id".to_string(), vec![]), gateway_id_tx))?;
+    let resp = gateway_id_rx.await??;
+    info!("Retrieved Gateway ID: {}", hex::encode(&resp));
+
+    let mut relay_id: [u8; 4] = [0; 4];
+    relay_id.copy_from_slice(&resp[4..]);
+    RELAY_ID
+        .set(Mutex::new(relay_id))
+        .map_err(|e| anyhow!("OnceCell error: {:?}", e))?;
+
+    // set CMD channel.
+
+    RELAY_CONCENTRATORD_CMD_CHAN
+        .set(cmd_tx)
+        .map_err(|e| anyhow!("OnceCell error: {:?}", e))?;
+
+    // Setup ZMQ event.
+
+    let (event_tx, event_rx) = mpsc::unbounded_channel::<Event>();
+
+    // Spawn the zmq event handler to a dedicated thread;
+    thread::spawn({
+        let event_url = conf.backend.relay_concentratord.event_url.clone();
+
+        move || {
+            let zmq_ctx = zmq::Context::new();
+            let mut sock = zmq_ctx.socket(zmq::SUB).unwrap();
+            sock.connect(&event_url).unwrap();
+            sock.set_subscribe("".as_bytes()).unwrap();
+
+            loop {
+                match receive_zmq_event(&mut sock) {
+                    Ok(v) => event_tx.send(v).unwrap(),
+                    Err(e) => {
+                        error!("Error receiving ZMQ event, error: {}", e);
+                    }
+                }
+            }
+        }
+    });
+
+    // Spawn event handler.
     tokio::spawn({
         let border_gateway = conf.relay.border_gateway;
 
         async move {
-            relay_event_loop(border_gateway, event_sock).await;
+            relay_event_loop(border_gateway, event_rx).await;
         }
     });
 
-    RELAY_CONCENTRATORD
-        .set(Mutex::new(b))
-        .map_err(|_| anyhow!("OnceCell set error"))?;
-
     Ok(())
-}
-
-struct Backend {
-    cmd_sock: zeromq::ReqSocket,
-    // cmd_sock: Arc<Mutex<zmq::Socket>>,
-    gateway_id: Option<[u8; 8]>,
-}
-
-impl Backend {
-    async fn read_gateway_id(&mut self) -> Result<()> {
-        trace!("Reading gateway ID");
-
-        self.cmd_sock
-            .send(
-                vec![bytes::Bytes::from("gateway_id"), bytes::Bytes::from("")]
-                    .try_into()
-                    .map_err(|e| anyhow!("To ZMQ message error: {}", e))?,
-            )
-            .await?;
-        let resp = timeout(Duration::from_millis(100), self.cmd_sock.recv()).await??;
-
-        let mut gateway_id: [u8; 8] = [0; 8];
-        gateway_id.copy_from_slice(&resp.get(0).map(|v| v.to_vec()).unwrap_or_default());
-
-        info!("Retrieved gateway ID: {}", hex::encode(gateway_id));
-
-        self.gateway_id = Some(gateway_id);
-
-        Ok(())
-    }
-
-    async fn send_command(&mut self, cmd: &str, b: &[u8]) -> Result<Vec<u8>> {
-        trace!("Sending command, cmd: {}, bytes: {}", cmd, hex::encode(b));
-
-        self.cmd_sock
-            .send(
-                vec![
-                    bytes::Bytes::from(cmd.to_string()),
-                    bytes::Bytes::from(b.to_vec()),
-                ]
-                .try_into()
-                .map_err(|e| anyhow!("To ZMQ message error: {}", e))?,
-            )
-            .await?;
-
-        timeout(Duration::from_millis(100), self.cmd_sock.recv())
-            .await?
-            .map(|v| v.get(0).map(|v| v.to_vec()).unwrap_or_default())
-            .map_err(|e| anyhow!("ZMQ error: {}", e))
-    }
 }
 
 async fn event_loop(
     border_gateway: bool,
     border_gateway_ignore_direct_uplinks: bool,
-    mut event_sock: zeromq::SubSocket,
+    mut event_rx: mpsc::UnboundedReceiver<Event>,
     filters: lrwn_filters::Filters,
 ) {
     trace!("Starting event loop");
-
-    loop {
-        trace!("Reading next event from zmq socket");
-        let resp = match event_sock.recv().await {
-            Ok(v) => v,
-            Err(e) => {
-                error!("Error receiving from zmq socket: {}", e);
-                sleep(Duration::from_secs(1)).await;
-                continue;
-            }
-        };
-
+    while let Some(event) = event_rx.recv().await {
         if let Err(e) = handle_event_msg(
             border_gateway,
             border_gateway_ignore_direct_uplinks,
-            &resp.get(0).map(|v| v.to_vec()).unwrap_or_default(),
-            &resp.get(1).map(|v| v.to_vec()).unwrap_or_default(),
+            &event,
             &filters,
         )
         .await
@@ -194,27 +230,10 @@ async fn event_loop(
     }
 }
 
-async fn relay_event_loop(border_gateway: bool, mut event_sock: zeromq::SubSocket) {
+async fn relay_event_loop(border_gateway: bool, mut event_rx: mpsc::UnboundedReceiver<Event>) {
     trace!("Starting relay event loop");
-
-    loop {
-        trace!("Reading next relay event from zmq socket");
-        let resp = match event_sock.recv().await {
-            Ok(v) => v,
-            Err(e) => {
-                error!("Error receiving from zmq socket: {}", e);
-                sleep(Duration::from_secs(1)).await;
-                continue;
-            }
-        };
-
-        if let Err(e) = handle_relay_event_msg(
-            border_gateway,
-            &resp.get(0).map(|v| v.to_vec()).unwrap_or_default(),
-            &resp.get(1).map(|v| v.to_vec()).unwrap_or_default(),
-        )
-        .await
-        {
+    while let Some(event) = event_rx.recv().await {
+        if let Err(e) = handle_relay_event_msg(border_gateway, &event).await {
             error!("Handle relay event error: {}", e);
             continue;
         }
@@ -224,21 +243,18 @@ async fn relay_event_loop(border_gateway: bool, mut event_sock: zeromq::SubSocke
 async fn handle_event_msg(
     border_gateway: bool,
     border_gateway_ignore_direct_uplinks: bool,
-    event: &[u8],
-    pl: &[u8],
+    event: &Event,
     filters: &lrwn_filters::Filters,
 ) -> Result<()> {
-    let event = String::from_utf8(event.to_vec())?;
-
     trace!(
         "Handling event, event: {}, data: {}",
-        event,
-        hex::encode(pl)
+        event.0,
+        hex::encode(&event.1)
     );
 
-    match event.as_str() {
+    match event.0.as_str() {
         "up" => {
-            let pl = gw::UplinkFrame::decode(pl)?;
+            let pl = gw::UplinkFrame::decode(event.1.as_slice())?;
 
             if let Some(rx_info) = &pl.rx_info {
                 // Filter out frames with invalid CRC.
@@ -279,7 +295,7 @@ async fn handle_event_msg(
         }
         "stats" => {
             if border_gateway {
-                let pl = gw::GatewayStats::decode(pl)?;
+                let pl = gw::GatewayStats::decode(event.1.as_slice())?;
                 info!("Gateway stats received, gateway_id: {}", pl.gateway_id);
                 proxy::send_stats(&pl).await?;
             }
@@ -292,18 +308,16 @@ async fn handle_event_msg(
     Ok(())
 }
 
-async fn handle_relay_event_msg(border_gateway: bool, event: &[u8], pl: &[u8]) -> Result<()> {
-    let event = String::from_utf8(event.to_vec())?;
-
+async fn handle_relay_event_msg(border_gateway: bool, event: &Event) -> Result<()> {
     trace!(
         "Handling relay event, event: {}, data: {}",
-        event,
-        hex::encode(pl)
+        event.0,
+        hex::encode(&event.1)
     );
 
-    match event.as_str() {
+    match event.0.as_str() {
         "up" => {
-            let pl = gw::UplinkFrame::decode(pl)?;
+            let pl = gw::UplinkFrame::decode(event.1.as_slice())?;
 
             if let Some(rx_info) = &pl.rx_info {
                 // Filter out frames with invalid CRC.
@@ -337,12 +351,13 @@ async fn send_command(cmd: &str, b: &[u8]) -> Result<Vec<u8>> {
         hex::encode(b)
     );
 
-    let backend = CONCENTRATORD
+    let cmd_chan = CONCENTRATORD_CMD_CHAN
         .get()
-        .ok_or_else(|| anyhow!("CONCENTRATORD is not set"))?;
-    let mut backend = backend.lock().await;
+        .ok_or_else(|| anyhow!("CONCENTRATORD_CMD_CHAN is not set"))?;
 
-    backend.send_command(cmd, b).await
+    let (cmd_tx, cmd_rx) = oneshot::channel::<Result<Vec<u8>>>();
+    cmd_chan.send(((cmd.to_string(), b.to_vec()), cmd_tx))?;
+    cmd_rx.await?
 }
 
 async fn send_relay_command(cmd: &str, b: &[u8]) -> Result<Vec<u8>> {
@@ -352,12 +367,13 @@ async fn send_relay_command(cmd: &str, b: &[u8]) -> Result<Vec<u8>> {
         hex::encode(b)
     );
 
-    let backend = RELAY_CONCENTRATORD
+    let cmd_chan = RELAY_CONCENTRATORD_CMD_CHAN
         .get()
-        .ok_or_else(|| anyhow!("RELAY_CONCENTRATORD is not set"))?;
-    let mut backend = backend.lock().await;
+        .ok_or_else(|| anyhow!("RELAY_CONCENTRATORD_CMD_CHAN is not set"))?;
 
-    backend.send_command(cmd, b).await
+    let (cmd_tx, cmd_rx) = oneshot::channel::<Result<Vec<u8>>>();
+    cmd_chan.send(((cmd.to_string(), b.to_vec()), cmd_tx))?;
+    cmd_rx.await?
 }
 
 pub async fn relay(pl: &gw::DownlinkFrame) -> Result<()> {
@@ -395,25 +411,55 @@ pub async fn send_gateway_configuration(pl: &gw::GatewayConfiguration) -> Result
 pub async fn get_relay_id() -> Result<[u8; 4]> {
     trace!("Getting relay ID");
 
-    let backend = RELAY_CONCENTRATORD
+    Ok(RELAY_ID
         .get()
-        .ok_or_else(|| anyhow!("RELAY_CONCENTRATORD is not set"))?;
-    let backend = backend.lock().await;
-
-    let mut relay_id: [u8; 4] = [0; 4];
-    relay_id.copy_from_slice(&backend.gateway_id.unwrap_or_default()[4..]);
-    Ok(relay_id)
+        .ok_or_else(|| anyhow!("RELAY_ID is not set"))?
+        .lock()
+        .await
+        .clone())
 }
 
 pub async fn get_gateway_id() -> Result<[u8; 8]> {
     trace!("Getting gateway ID");
 
-    let backend = CONCENTRATORD
+    Ok(GATEWAY_ID
         .get()
-        .ok_or_else(|| anyhow!("CONCENTRATORD is not set"))?;
-    let backend = backend.lock().await;
+        .ok_or_else(|| anyhow!("GATEWAY_ID is not set"))?
+        .lock()
+        .await
+        .clone())
+}
 
-    let mut gateway_id: [u8; 8] = [0; 8];
-    gateway_id.copy_from_slice(&backend.gateway_id.unwrap_or_default());
-    Ok(gateway_id)
+fn send_zmq_command(sock: &mut zmq::Socket, cmd: &Command) -> Result<Vec<u8>> {
+    debug!(
+        "Sending command to socket, command: {}, payload: {}",
+        &cmd.0 .0,
+        hex::encode(&cmd.0 .1)
+    );
+
+    sock.send(&cmd.0 .0, zmq::SNDMORE)?;
+    sock.send(&cmd.0 .1, 0)?;
+
+    // set poller so that we can timeout after 100ms
+    let mut items = [sock.as_poll_item(zmq::POLLIN)];
+    zmq::poll(&mut items, 100)?;
+    if !items[0].is_readable() {
+        return Err(anyhow!("Could not read down response"));
+    }
+
+    // red tx ack response
+    let resp_b: &[u8] = &sock.recv_bytes(0)?;
+    Ok(resp_b.to_vec())
+}
+
+fn receive_zmq_event(sock: &mut zmq::Socket) -> Result<Event> {
+    let msg = sock.recv_multipart(0)?;
+    if msg.len() != 2 {
+        return Err(anyhow!("Event must have 2 frames"));
+    }
+
+    let event = String::from_utf8(msg[0].to_vec())?;
+    let b = msg[1].to_vec();
+
+    Ok((event, b))
 }
